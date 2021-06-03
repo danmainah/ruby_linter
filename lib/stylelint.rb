bdd64714fa377334922b7eeebb0b520f24e1b2e7
danmainah <danielmainawaweru@gmail.com>
2021-06-03 14:24:27 +0300

    Add linter check methods for specific errors

---
+++ lib/stylelint.rb
@@ -0,0 +1,27 @@
+module Lintcheck
+  def check_trailing_spaces(line, index)
+    if !line.strip.empty? && line[-2] == ' '
+      @errors << "Line #{index} ends with a trailing white space".colorize(:light_red)
+    end
+
+    def empty_line(line, index)
+      @errors << "Empty line on line #{index}" if line.strip.empty?
+    end
+
+    def empty_end_line(line, index)
+      @errors << "Add an empty line after line  #{index}" if line.end_with?('end')
+    end
+
+    def check_line_length(line, index)
+      return unless line.length > 50
+
+      @errors << "Line #{index} it too long make it shorter"
+    end
+
+    def empty_start_line(line, _index)
+      return unless line.strip.split.first.eql?('class')
+
+      @errors << 'Extra empty line detected at the beginning'
+    end
+  end
+end
